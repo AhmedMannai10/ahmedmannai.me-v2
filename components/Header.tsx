05febc910b86abad4a2504c7718dcd56d6bbbbbb
DrGhost <mannaiahmed7518@gmail.com>
2024-08-01 18:55:33 +0100

    updating the favicon images

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,11 +12,11 @@ const Header = () => {
       <div>
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
-            <div className="mr-3">
+            {/*<div className="mr-3">
               <Logo />
-            </div>
+            </div>*/}
             {typeof siteMetadata.headerTitle === 'string' ? (
-              <div className="hidden h-6 text-2xl font-semibold sm:block">
+              <div className="hidden h-7 bg-gradient-to-r from-purple-600 to-pink-400  bg-clip-text text-2xl  font-bold text-transparent sm:block">
                 {siteMetadata.headerTitle}
               </div>
             ) : (
