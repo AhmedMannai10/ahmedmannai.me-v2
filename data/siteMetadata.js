7211767327288ddfbacb1240a51feeb9fff5a717
Ahmed Mannai <77109198+AhmedMannai10@users.noreply.github.com>
2025-05-11 12:01:36 +0100

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -13,7 +13,7 @@ const siteMetadata = {
   //mastodon: 'https://mastodon.social/@mastodonuser',
   email: 'ahmedmannai.com@gmail.com',
   github: 'https://github.com/ahmedmannai10',
-  x: 'https://x.com/Ahmed_Mannai_10',
+  x: 'https://x.com/Ahmad_Manai',
   // twitter: 'https://twitter.com/Twitter',
   facebook: 'https://facebook.com',
   youtube: 'https://youtube.com/@ahmed-mannai',
